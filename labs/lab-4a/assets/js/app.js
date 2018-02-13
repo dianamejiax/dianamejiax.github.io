4b2de4cb0685f8360f9adf3eaab23297000d8306
Diana Mejia <dianamejia@Dianas-MBP.wireless.newschool.edu>
2018-02-13 11:36:51 -0500

    Lab 4a + Lab 4b

---
+++ labs/lab-4a/assets/js/app.js
@@ -0,0 +1,20 @@
+function hello(name, age) {
+  console.log('Name', name);
+  console.log('Age', age);
+}
+
+hello('Diana Mejia', 22);
+
+
+
+//these are global variables
+// var first_name = 'Diana',
+//     last_name  = 'Mejia',
+//     full_name  = first_name + ' ' + last_name;
+
+
+// function hello() {
+//   alert(full_name);
+// }
+
+// hello(); 
