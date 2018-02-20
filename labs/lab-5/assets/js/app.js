10a8d503409737b940dede59199a2171f62082ff
Diana Mejia <dianamejia@Dianas-MBP.wireless.newschool.edu>
2018-02-20 10:37:37 -0500

    enable user to toggle cart

--- labs/lab-5/assets/js/app.js
+++ labs/lab-5/assets/js/app.js
@@ -1,12 +1,7 @@
 $('.item').click(function() {
-
-
+    
 if (!$(this).hasClass('in-cart')){
-
-
     $(this).addClass('in-cart');
-  
-
 
     var current_count = $('.item-count'). html(),
         current_value = Math.abs(current_count),
@@ -15,4 +10,8 @@ if (!$(this).hasClass('in-cart')){
     $('.item-count').html(new_count);
 }
 
+});
+
+$('.cart-toggle').click(function(){
+    $('.cart').toggleClass('hide');
 });
