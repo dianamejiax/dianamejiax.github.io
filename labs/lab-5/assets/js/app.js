c994c702e21f05e6ee015a4a12f57a95eace1bc9
Diana Mejia <dianamejia@Dianas-MBP.wireless.newschool.edu>
2018-02-20 11:37:08 -0500

    Dynamically add items to cart

--- labs/lab-5/assets/js/app.js
+++ labs/lab-5/assets/js/app.js
@@ -1,13 +1,14 @@
 $('.item').click(function() {
     
 if (!$(this).hasClass('in-cart')){
-    $(this).addClass('in-cart');
-
     var current_count = $('.item-count'). html(),
         current_value = Math.abs(current_count),
         new_count     = current_value + 1;
         
     $('.item-count').html(new_count);
+    $(this).addClass('in-cart');
+    //add item to .cart
+    add_to_cart(this);
 }
 
 });
@@ -15,3 +16,13 @@ if (!$(this).hasClass('in-cart')){
 $('.cart-toggle').click(function(){
     $('.cart').toggleClass('hide');
 });
+
+function add_to_cart(line_item){
+//get some img and price of item clicked
+//inject html with img an price
+var price = $(line_item).data('price'),
+    img   = $(line_item).data('img'),
+    line_item_html = '<div class="line-item"><div class="line-item-img ' + img + '"></div><div class="line-item-price">' + price + '</div></div>';
+
+$ ('.line-items').append(line_item_html);
+}
